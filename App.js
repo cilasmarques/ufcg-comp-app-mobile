7e13fb3fbb63e133d93ebaaa2d273510a487426c
cilasmarques <cilas.marques@ccc.ufcg.edu.br>
2023-02-21 18:07:45 -0300

    Updating expo configurations

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
-import MainRouter from "./src/routes/MainRouter";
+import { StatusBar } from 'expo-status-bar';
+
+// CONTEXT
 import { AuthProvider } from "./src/context/AuthContext";
+
+// ROUTER
+import MainRouter from "./src/routes/MainRouter";
 
 export default function App() {
   return (
     <AuthProvider>
+      <StatusBar style="auto" />
       <MainRouter/>
     </AuthProvider>
   );
